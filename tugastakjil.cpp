2c6bd23abfacee6576754cf740417964f77f5c4b
Ilasbintang <ilasbintang727@gmail.com>
2026-03-02 21:42:32 +0700

    first commit

---
+++ tugastakjil.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+float d1, d2;
+
+void ProsedurInputData() {
+    cout << "Masukkan diagonal 1 : ";
+    cin >> d1;
+    cout << "Masukkan diagonal 2 : ";
+    cin >> d2;
+}
+
+float ProsedurHitungLuasBerparameter(float d1, float d2) {
+    return 0.5 * d1 * d2;
+}
+
+void ProsedurOutput() {
+    cout << "Luas Ketupat : " << ProsedurHitungLuasBerparameter(d1, d2);
+}
+
+int main() {
+    ProsedurInputData();
+    ProsedurOutput();
+}
